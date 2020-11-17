f8dddaecaa42d18af573f64175b387a02ad270e3
Maël Drapier <mael.drapier@epitech.eu>
2020-11-17 18:52:10 +0100

    replacing class by className

--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -6,10 +6,10 @@ function GameOver(props) {
             style={{width: props.width, height: props.height, borderWidth: props.width / 50}}>
             <div className='GameOver'
                 style={{fontSize: props.width / 15}}>
-                <div class="GameOverText">GAME OVER</div>
+                <div className="GameOverText">GAME OVER</div>
                 <div>Your score: {props.score}</div>
                 <div>{props.newHighScore ? 'New local ' : 'Local '}high score: {props.highScore}</div>
-                <div class="PressSpaceText">Press Space to restart</div>
+                <div className="PressSpaceText">Press Space to restart</div>
             </div>
         </div>
     )
