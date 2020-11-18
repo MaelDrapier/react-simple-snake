63ce4ab5f5893d2001e1132f436ac2fb446d7b1b
Maël Drapier <mael.drapier@epitech.eu>
2020-11-18 14:31:34 +0100

    updating index.js for github pages

--- demo/index.js
+++ demo/index.js
@@ -7,11 +7,14 @@ ReactDOM.render(
   <React.StrictMode>
     <h1>react-simple-snake</h1>
     <div className="textWrapper">
+      <p className="subTitle">
+      <a href="https://www.npmjs.com/package/react-simple-snake" target="_blank" rel="noopener noreferrer">NPM</a> -
+      <a href="https://github.com/MaelDrapier/react-simple-snake" target="_blank" rel="noopener noreferrer"> Github</a>
+      </p>
       <p id="instructions">Use the arrow keys or W/A/S/D to play</p>
       <p className="subTitle">A simple snake game created with the
       <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"> React </a>
-      framework, available
-      <a href="https://www.npmjs.com/package/react-simple-snake" target="_blank" rel="noopener noreferrer"> here</a></p>
+      framework</p>
     </div>
     <SnakeGame />
   </React.StrictMode>,
