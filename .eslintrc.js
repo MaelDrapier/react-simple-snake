5675df4a0e6b0b7da8cc5e097587386caff19253
Maël Drapier <mael.drapier@epitech.eu>
2020-11-19 21:59:26 +0100

    adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'prettier/prettier': ['error'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    }
+  }
+}
