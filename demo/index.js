1a7876aa9e6c454a271bd27739d38e647c1bbeb6
monicabansal91 <35876882+monicabansal91@users.noreply.github.com>
2023-02-20 21:43:27 -0500

    updating the title

--- demo/index.js
+++ demo/index.js
@@ -5,16 +5,9 @@ import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>react-simple-snake</h1>
+    <h1>Play Snake Game</h1>
     <div className="textWrapper">
-      <p className="subTitle">
-      <a href="https://www.npmjs.com/package/react-simple-snake" target="_blank" rel="noopener noreferrer">NPM</a> -
-      <a href="https://github.com/MaelDrapier/react-simple-snake" target="_blank" rel="noopener noreferrer"> Github</a>
-      </p>
       <p id="instructions">Use the arrow keys or W/A/S/D to play</p>
-      <p className="subTitle">A simple snake game created with the
-      <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"> React </a>
-      JavaScript library</p>
     </div>
     <SnakeGame />
   </React.StrictMode>,
